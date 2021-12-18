1b533f33fde6ec13c418831df169e57f407068a5
Abdul Momin <abdulmomin114477@gmail.com>
2021-12-18 12:31:49 +0600

    Added data types

--- src/Types.ts
+++ src/Types.ts
@@ -15,3 +15,41 @@ export interface ProductPreviewType extends Product {
 export interface CartItem extends Product {
   quantity: number;
 }
+
+interface BaseProduct {
+  id: string;
+  name: string;
+  price: number;
+  category: string;
+  thumbnail: string;
+}
+
+export interface ProductPreviewTypee extends BaseProduct {
+  oldPrice?: number;
+  image: string;
+  createdAt: string;
+}
+
+export interface CartItemm extends BaseProduct {
+  quantity: string;
+}
+
+export interface Review {
+  //
+}
+
+export interface Question {
+  //
+}
+
+export interface Reply {
+  //
+}
+
+export interface FullProduct extends BaseProduct {
+  images: string[];
+  description: string;
+  reviews: Review[];
+  question: Question[];
+  replies: Reply[];
+}
